1ca6002b865fba9dedc9889af0954b71efcb7c83
KizMistry <Kiz_mistry@hotmail.com>
2023-06-30 19:13:50 +0000

    add NavBar.js, logo, bootstrap, fonts
    added standard bootstrap Navbar code and logo to asset folder

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/taskflow-logo.png";
+
+const NavBar = () => {
+  return (
+    <Container>
+      <Navbar bg="light" expand="md" fixed="top">
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="Home">
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link href="Sign in">
+              <i className="fas fa-sign-in-alt"></i>Link
+            </Nav.Link>
+            <Nav.Link href="Sign up">
+              <i className="fas fa-user-plus"></i>Link
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
+};
+
+export default NavBar;
