e3a0346d94fca0a74304c35b4fc135eac96da9e4
KizMistry <Kiz_mistry@hotmail.com>
2023-07-01 18:26:42 +0000

    add button and signup css modules

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults';
 
 function App() {
   return (
