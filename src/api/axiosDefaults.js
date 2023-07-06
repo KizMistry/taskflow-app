a72d7cfdad3a97afa7b427f785f3bf796c24c01e
KizMistry <Kiz_mistry@hotmail.com>
2023-07-06 18:16:26 +0000

    add axios interceptors
    add interceptors to refresh access tokens.
    add useMemo to CurrentUserContext.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://taskflow-app-734253c0080e.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
