ec99aa3ffe3403282faab66955892d0c0acd3c2c
KizMistry <Kiz_mistry@hotmail.com>
2023-07-07 21:17:35 +0000

    add useEffect function to ProjectPage.js
    add useEffect to request data for specific project id

--- src/pages/projects/ProjectPage.js
+++ src/pages/projects/ProjectPage.js
@@ -1,73 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function ProjectPage() {
-  // Add your logic here
+  const { id } = useParams();
+  const [project, setProject] = useState({ results: [] });
 
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: project }] = await Promise.all([
+          axiosReq.get(`/projects/${id}`),
+        ]);
+        setProject({ results: [project] });
+        console.log(project);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount();
+  }, [id]);
 
   return (
     <Container>
       <Row>
         <Col>
-          <div
-            className="bg-light p-2"
-
-          >
+          <div className="bg-light p-2">
             <h2>To Do</h2>
 
-                <div
-                  key="task.id"
-                  className="card mb-2"
-                  draggable
-                >
-                  <div className="card-body">task.title</div>
-                </div>
-
+            <div key="task.id" className="card mb-2" draggable>
+              <div className="card-body">task.title</div>
+            </div>
           </div>
         </Col>
         <Col>
-          <div
-            className="bg-light p-2"
-
-          >
+          <div className="bg-light p-2">
             <h2>In Progress</h2>
-            
-                <div
-                  key="{task.id}"
-                  className="card mb-2"
-                  draggable
 
-                >
-                  <div className="card-body">task.title</div>
-                </div>
-          
+            <div key="{task.id}" className="card mb-2" draggable>
+              <div className="card-body">task.title</div>
+            </div>
           </div>
         </Col>
         <Col>
-          <div
-            className="bg-light p-2"
-
-          >
+          <div className="bg-light p-2">
             <h2>Done</h2>
-          
-                <div
-                  key="{task.id}"
-                  className="card mb-2"
-                  draggable
-                >
-                  <div className="card-body">task.title</div>
-                </div>
-             
+
+            <div key="{task.id}" className="card mb-2" draggable>
+              <div className="card-body">task.title</div>
+            </div>
           </div>
         </Col>
       </Row>
     </Container>
   );
-};
+}
 
 export default ProjectPage;
