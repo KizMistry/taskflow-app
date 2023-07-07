8622909c4316ba2375857da40a62d8101bf08503
KizMistry <Kiz_mistry@hotmail.com>
2023-07-07 17:30:26 +0000

    add ProjectCreateForm.js page
    added basic template for the page, add path in app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import ProjectCreateForm from "./pages/projects/ProjectCreateForm";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
               <Route exact path="/" render={() => <h1>Home Page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route exact path="/projects/create" render={() => <ProjectCreateForm />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
