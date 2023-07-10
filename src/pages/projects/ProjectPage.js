d1cb221d0250aac67a93bf56a913718363f44510
KizMistry <Kiz_mistry@hotmail.com>
2023-07-10 20:20:22 +0000

    add Note form component to ProjectPage
    added form and notes display to project page

--- src/pages/projects/ProjectPage.js
+++ src/pages/projects/ProjectPage.js
@@ -9,19 +9,26 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Project from "./Project";
 
+import NoteCreateForm from "../notes/NoteCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Note from "../notes/Note";
+
 function ProjectPage() {
   const { id } = useParams();
   const [project, setProject] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [notes, setNotes] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: project }] = await Promise.all([
+        const [{ data: project }, {data: notes}] = await Promise.all([
           axiosReq.get(`/projects/${id}`),
+          axiosReq.get(`/notes/?project=${id}`)
         ]);
         setProject({ results: [project] });
-        console.log(project);
-        console.log(project.description);
+        setNotes(notes);
       } catch (err) {
         console.log(err);
       }
@@ -30,19 +37,45 @@ function ProjectPage() {
   }, [id]);
 
   return (
-    <Container className={appStyles.Content}>
+    <>
       <Row className="h-100">
-        <Col md={12} lg={12} >
-      <Project {...project.results[0]} setProjects={setProject} projectPage />
-      </Col>
-        </Row>
-        <Row className="h-100">
+        <Col md={12} lg={12}>
+          <Project
+            {...project.results[0]}
+            setProjects={setProject}
+            projectPage
+          />
+          <Container className={appStyles.Content}>
+            {currentUser ? (
+              <NoteCreateForm
+                profile_id={currentUser.profile_id}
+                profileImage={profile_image}
+                project={id}
+                setProject={setProject}
+                setNotes={setNotes}
+              />
+            ) : notes.results.length ? (
+              "Notes"
+            ) : null}
+            {notes.results.length ? (
+              notes.results.map(note => (
+                <Note key={note.id} {...note} />
+              ))
+            ) : currentUser ? (
+              <span>No notes</span>
+            ) : (
+              <span>No notes yet</span>
+            )}
+          </Container>
+        </Col>
+      </Row>
+      <Row className="h-100">
         <Col>
           <div className="bg-light p-2">
             <h2>To Do</h2>
 
             <div key="task.id" className="card mb-2" draggable>
-              <div className="card-body">project.title</div>
+              <div className="card-body">task.title</div>
             </div>
           </div>
         </Col>
@@ -65,7 +98,7 @@ function ProjectPage() {
           </div>
         </Col>
       </Row>
-    </Container>
+    </>
   );
 }
 
