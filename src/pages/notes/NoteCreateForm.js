6ef0bb02c1ee6d3d17ff0fa257315e8dc847de0e
KizMistry <Kiz_mistry@hotmail.com>
2023-07-10 20:18:40 +0000

    add NoteCreateForm.js
    add note form and note create/edit form css

---
+++ src/pages/notes/NoteCreateForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+
+import styles from "../../styles/NotesCreateEditForm.module.css";
+import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
+
+function NoteCreateForm(props) {
+  const { project, setProject, setNotes, profileImage, profile_id } = props;
+  const [content, setContent] = useState("");
+
+  const handleChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axiosRes.post("/notes/", {
+        content,
+        project,
+      });
+      setNotes((prevNotes) => ({
+        ...prevNotes,
+        results: [data, ...prevNotes.results],
+      }));
+      setProject((prevProject) => ({
+        results: [
+          {
+            ...prevProject.results[0],
+            notes_count: prevProject.results[0].notes_count + 1,
+          },
+        ],
+      }));
+      setContent("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form className="mt-2" onSubmit={handleSubmit}>
+      <Form.Group>
+        <InputGroup>
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profileImage} />
+          </Link>
+          <Form.Control
+            className={styles.Form}
+            placeholder="my note..."
+            as="textarea"
+            value={content}
+            onChange={handleChange}
+            rows={2}
+          />
+        </InputGroup>
+      </Form.Group>
+      <button
+        className={`${styles.Button} btn d-block ml-auto`}
+        disabled={!content.trim()}
+        type="submit"
+      >
+        post
+      </button>
+    </Form>
+  );
+}
+
+export default NoteCreateForm;
