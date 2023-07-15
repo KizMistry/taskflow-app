6c43b77e071d18bcf7b1cd108d754757b23c77ff
KizMistry <Kiz_mistry@hotmail.com>
2023-07-15 23:53:45 +0000

    install jwt and add 3 jwt functions to utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,16 +1,30 @@
-import { axiosReq } from "../api/axiosDefaults"
+import jwtDecode from "jwt-decode";
+import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
-    try {
-        const {data} = await axiosReq.get(resource.next)
-        setResource(prevResource => ({
-            ...prevResource,
-            next:data.next,
-            results: data.results.reduce((acc, cur) => {
-                return acc.some(accResult => accResult.id === cur.id) ? acc : [...acc, cur]
-            }, prevResource.results)
-        }))
-    } catch(err){
+  try {
+    const { data } = await axiosReq.get(resource.next);
+    setResource((prevResource) => ({
+      ...prevResource,
+      next: data.next,
+      results: data.results.reduce((acc, cur) => {
+        return acc.some((accResult) => accResult.id === cur.id)
+          ? acc
+          : [...acc, cur];
+      }, prevResource.results),
+    }));
+  } catch (err) {}
+};
 
-    }
-}
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !!localStorage.getItem("refreshTokenTimestamp");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refreshTokenTimestamp");
+};
