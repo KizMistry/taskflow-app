0f0580adf36197864c21abad7007276e303ac2d6
KizMistry <Kiz_mistry@hotmail.com>
2023-07-15 23:04:02 +0000

    update ProjectsPage.js
    add check currentUser on page render

--- src/pages/projects/ProjectsPage.js
+++ src/pages/projects/ProjectsPage.js
@@ -7,7 +7,7 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/ProjectsPage.module.css";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Project from "./Project";
 
@@ -15,11 +15,13 @@ import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function ProjectsPage({ message, filter = "" }) {
   const [projects, setProjects] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+  const currentUser = useCurrentUser();
 
   const [query, setQuery] = useState("");
 
@@ -45,54 +47,73 @@ function ProjectsPage({ message, filter = "" }) {
   }, [filter, query, pathname]);
 
   return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={12}>
-        <i className={`fas fa-search ${styles.searchIcon}`} />
-        <Form
-          className={styles.SearchBar}
-          onSubmit={(event) => event.preventDefault()}
-        >
-          <Form.Control
-            value={query}
-            onChange={(event) => setQuery(event.target.value)}
-            type="text"
-            className="mr-sm-2"
-            placeholder="Search your Projects"
-          />
-        </Form>
-        <p>Your projects listed from newest to oldest:</p>
-        {hasLoaded ? (
-          <>
-            {projects.results.length ? (
-                <InfiniteScroll
-                children={
-                    projects.results.map((project) => (
+    <>
+      {currentUser ? (
+        <>
+          <Row className="h-100">
+            <Col className="py-2 p-0 p-lg-2" lg={12}>
+              <i className={`fas fa-search ${styles.searchIcon}`} />
+              <Form
+                className={styles.SearchBar}
+                onSubmit={(event) => event.preventDefault()}
+              >
+                <Form.Control
+                  value={query}
+                  onChange={(event) => setQuery(event.target.value)}
+                  type="text"
+                  className="mr-sm-2"
+                  placeholder="Search your Projects"
+                />
+              </Form>
+              <p>Your projects listed from newest to oldest:</p>
+              {hasLoaded ? (
+                <>
+                  {projects.results.length ? (
+                    <InfiniteScroll
+                      children={projects.results.map((project) => (
                         <Project
                           key={project.id}
                           {...project}
                           setProjects={setProjects}
                         />
-                      ))
-                }
-                dataLength={projects.results.length}
-                loader={<Asset spinner />}
-                hasMore={!!projects.next}
-                next={() => fetchMoreData(projects, setProjects)}
-                />
-              
-            ) : (
-              <Container className={appStyles.Content}>
-                <Asset src={NoResults} message={message} />
-              </Container>
-            )}
-          </>
-        ) : (
+                      ))}
+                      dataLength={projects.results.length}
+                      loader={<Asset spinner />}
+                      hasMore={!!projects.next}
+                      next={() => fetchMoreData(projects, setProjects)}
+                    />
+                  ) : (
+                    <Container className={appStyles.Content}>
+                      <Asset
+                        src={NoResults}
+                        message={"Get started by creating a project"}
+                      />
+                      <Link
+                        className={styles.NavLink}
+                        activeClassName={styles.Active}
+                        to="/projects/create"
+                      >
+                        <i className="fas fa-plus-square"></i>Create Project
+                      </Link>
+                    </Container>
+                  )}
+                </>
+              ) : (
+                <Container className={appStyles.Content}>
+                  <Asset spinner />
+                </Container>
+              )}
+            </Col>
+          </Row>
+        </>
+      ) : (
+        <>
           <Container className={appStyles.Content}>
-            <Asset spinner />
+            <div>Log in to get started</div>
           </Container>
-        )}
-      </Col>
-    </Row>
+        </>
+      )}
+    </>
   );
 }
 
